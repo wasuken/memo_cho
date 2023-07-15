3cd40b5cbf7e0fd7081a1237101aae48a6204591
wasuken <wevorence@gmail.com>
2023-07-15 16:36:00 +0900

    終わり

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,113 @@
 import { useState } from "react";
 import Memo from "./components/Memo";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+
+type Memo = {
+  id: number;
+  contents: string;
+  tags: string[];
+};
 
 function App() {
+  const [memoList, setMemoList] = useState<Memo[]>([]);
+  const updateMemo = (id: number, s: string) => {
+    const nlist = [...memoList];
+    for (let i = 0; i < nlist.length; i++) {
+      if (nlist[i].id === id) {
+        nlist[i].contents = s;
+        break;
+      }
+    }
+    setMemoList([...nlist]);
+  };
+  const handleDeleteMemo = (id: number) => {
+    const memo = memoList.find((m) => m.id === id);
+    if (!memo) {
+      console.log("error", `not found: ${id}`);
+      return;
+    }
+    setMemoList([...memoList.filter((m) => m.id !== id)]);
+  };
+  const handleAddMemo = () => {
+    setMemoList([...memoList, { id: Date.now(), contents: "", tags: [] }]);
+  };
+  const handleTagInputChange = (id: number, s: string) => {
+    const nlist = [...memoList];
+    for (let i = 0; i < nlist.length; i++) {
+      if (nlist[i].id === id) {
+        const ts = Array.from(
+          new Set(
+            [...nlist[i].tags, ...s.split(" ")].filter((x) => x.length > 1)
+          )
+        );
+        nlist[i].tags = ts;
+        break;
+      }
+    }
+    setMemoList([...nlist]);
+  };
+  const handleTagAllDelete = (id: number) => {
+    const nlist = [...memoList];
+    for (let i = 0; i < nlist.length; i++) {
+      if (nlist[i].id === id) {
+        nlist[i].tags = [];
+        break;
+      }
+    }
+    setMemoList([...nlist]);
+  };
+  const clearMoved = () => {
+    setMemoList(
+      [...memoList].map((m) => {
+        return { ...m, isMoved: false };
+      })
+    );
+  };
+  const moveMemoToTopByTag = (tag: string) => {
+    const mList = [...memoList];
+    const idList = mList
+      .filter((memo) => memo.tags.includes(tag))
+      .map((x) => x.id);
+    for (const id of idList) {
+      const memoIndex = mList.findIndex((m) => m.id === id);
+      if (memoIndex !== -1) {
+        const memo = mList[memoIndex];
+        mList.splice(memoIndex, 1);
+        mList.unshift(memo);
+      }
+      setTimeout(() => {
+        clearMoved();
+      }, 5000);
+    }
+
+    setMemoList(mList);
+  };
+
   return (
-    <>
-      <Memo />
-      <Memo />
-    </>
+    <Container>
+      <div>
+        <Button variant="primary" onClick={handleAddMemo}>
+          Create
+        </Button>
+      </div>
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "30px" }}>
+        {memoList.map((memo) => (
+          <Memo
+            key={memo.id}
+            id={memo.id}
+            tags={memo.tags}
+            contents={memo.contents}
+            onChange={(s) => updateMemo(memo.id, s)}
+            onDelete={handleDeleteMemo}
+            onTagInputChange={(s) => handleTagInputChange(memo.id, s)}
+            onTagAllDeleteClick={() => handleTagAllDelete(memo.id)}
+            onTagClick={moveMemoToTopByTag}
+          />
+        ))}
+      </div>
+    </Container>
   );
 }
 
--- src/components/Memo.tsx
+++ src/components/Memo.tsx
@@ -1,8 +1,87 @@
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
-export default function Memo() {
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import CloseButton from "react-bootstrap/CloseButton";
+import Button from "react-bootstrap/Button";
+import "./memo.css";
+
+interface IProps {
+  id: number;
+  contents: string;
+  tags: string[];
+  onChange: (s: string) => void;
+  onDelete: (id: number) => void;
+  onTagInputChange: (s: string) => void;
+  onTagAllDeleteClick: () => void;
+  onTagClick: (tag: string) => void;
+}
+
+export default function Memo(props: IProps) {
+  const [tagsInput, setTagsInput] = useState<string>("");
   return (
-    <Card>
-      <Card.Body>This is some text within a card body.</Card.Body>
+    <Card style={{ width: "25vw" }}>
+      <Card.Header>
+        <CloseButton onClick={() => props.onDelete(props.id)} />
+      </Card.Header>
+      <Card.Body>
+        <FloatingLabel label="Memo">
+          <Form.Control
+            as="textarea"
+            placeholder="memo"
+            onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+              props.onChange(e.target.value)
+            }
+            value={props.contents}
+            style={{ height: "100px" }}
+          />
+        </FloatingLabel>
+      </Card.Body>
+      <Card.Body>
+        <Form.Control
+          type="text"
+          placeholder="tags"
+          value={tagsInput}
+          onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const s = e.target.value;
+            setTagsInput(s);
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            justifyContent: "flex-start",
+            paddingTop: "10px",
+          }}
+        >
+          <Button
+            variant="primary"
+            onClick={() => {
+              props.onTagInputChange(tagsInput);
+              setTagsInput("");
+            }}
+          >
+            Add
+          </Button>
+          <Button variant="danger" onClick={() => props.onTagAllDeleteClick()}>
+            Tags Clear
+          </Button>
+        </div>
+      </Card.Body>
+      {props.tags.length > 0 && (
+        <Card.Footer>
+          {props.tags.map((tag, i) => (
+            <Button
+              key={i}
+              variant="primary"
+              onClick={() => props.onTagClick(tag)}
+            >
+              {tag}
+            </Button>
+          ))}
+        </Card.Footer>
+      )}
     </Card>
   );
 }
